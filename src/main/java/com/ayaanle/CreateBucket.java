5bf42f5de57aebf71755c6cfe7abf582db6dc613
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2023-12-15 09:35:36 +0300

    Update CreateBucket.java

--- src/main/java/com/ayaanle/CreateBucket.java
+++ src/main/java/com/ayaanle/CreateBucket.java
@@ -1,1 +1,30 @@
 
+public class CreateBucket {
+    public static Bucket getBucket(String bucket_name) {
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        Bucket named_bucket = null;
+        List<Bucket> buckets = s3.listBuckets();
+        for (Bucket b : buckets) {
+            if (b.getName().equals(bucket_name)) {
+                named_bucket = b;
+            }
+        }
+        return named_bucket;
+    }
+
+    public static Bucket createBucket(String bucket_name) {
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        Bucket b = null;
+        if (s3.doesBucketExistV2(bucket_name)) {
+            System.out.format("Bucket %s already exists.\n", bucket_name);
+            b = getBucket(bucket_name);
+        } else {
+            try {
+                b = s3.createBucket(bucket_name);
+            } catch (AmazonS3Exception e) {
+                System.err.println(e.getErrorMessage());
+            }
+        }
+        return b;
+    }
+}
