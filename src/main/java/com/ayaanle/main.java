17482e4f4a758654c1d3401fc60fb11b9823a23e
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2023-12-15 09:48:41 +0300

    Update main.java

--- src/main/java/com/ayaanle/main.java
+++ src/main/java/com/ayaanle/main.java
@@ -1,7 +1,9 @@
 package com.ayaanle;
 
 import com.ayaanle.ListBuckets;
+import com.ayaanle.GetBuckets
 import java.util.Scanner;
+
 public class Main{
   public static void main(String [] args){
     
